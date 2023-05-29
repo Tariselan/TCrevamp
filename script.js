a4f71b7adb8fd5b91e2ffd974ef5ebff96417204
Tariselan <woodmoreaaron@gmail.com>
2023-05-29 17:33:12 +1200

    MAAAJOR fixes

--- script.js
+++ script.js
@@ -1,25 +1,41 @@
-var player = {
-    name: "Player",
-    actions: {
+// Classes
 
+class Enemy {
+    constructor(id, name, totHP, attacks) {
+        this.id = id;
+        this.name = name;
+        this.totHP = totHP; // total
+        this.curHP = totHP; // current
+        this.attacks = attacks;
     }
 }
-var enemies = {
-    test: Enemy("Test Enemy", 20, punch)
+
+class Attack {
+    constructor(id, name, damage, accuracy) {
+        this.id = id;
+        this.name = name;
+        this.damage = damage;
+        this.accuracy = accuracy;
+    }
 }
+
+// lists of things
 
 var attacks = {
-    punch: Attack("Punch", 2, 0.75)
+    punch: new Attack(1, "Punch", 2, 0.9),
+    bite: new Attack(2, "Bite", 4, 0.5),
+    kick: new Attack(3, "Kick", 3, 0.8)
 }
 
-function Enemy(name, totHP, attack1) {
-    this.name = name;
-    this.totHP = totHP;
-    this.attack1 = attack1;
+var enemies = {
+    test: new Enemy(0, "Test Enemy", 20, [attacks.punch]),
+    drunk: new Enemy(1, "Drunk Man", 45, [attacks.punch, attacks.bite])
 }
 
-function Attack(name, damage, accuracy) {
-    this.name = name;
-    this.damage = damage;
-    this.accuracy = accuracy;
+// the rest ig lol
+var player = {
+    name: "Player",
+    attacks: [attacks.punch],
+    totHP: 100,
+    curHP: 100
 }
