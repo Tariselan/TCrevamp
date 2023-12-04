07c09765e9d6ca8b8b00716f295beaba1917a08b
krv1974 <63528758+krv1974@users.noreply.github.com>
2023-12-04 18:52:45 +1300

    Update

--- script.js
+++ script.js
@@ -142,3 +142,13 @@ document.body.addEventListener('keypress', function(event) {
         }
     }
 })
+
+$(document).ready(function(){
+    setTimeout(PlayMusic,3000);
+  })
+  function PlayMusic() {
+
+    var play=document.getElementById("music");
+    play.play();
+  }
+  
