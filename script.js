6aece3e1a2fae3b3de622c0adda4e8fd0cc7671a
krv1974 <63528758+krv1974@users.noreply.github.com>
2023-12-18 16:49:10 +1300

    suprise the trans lesbian is woke

--- script.js
+++ script.js
@@ -49,13 +49,27 @@ var materials = {
 
 // the rest ig lol
 var player = {
-    name: "Player",
+    name: "Freya",
     attacks: [attacks.punch],
     inventory: [],
     totHP: 100,
     curHP: 100
 }
+
+const pronouns = [
+    "(she/her)",
+    "(he/him)",
+    "(they/them)"
+];
+
+var currentpronouns = 0; // 0 == she/her, 1 = they/them, 2 = 
+document.getElementById("playerpronouns").addEventListener("click", function changepronouns() {
+    currentpronouns = (currentpronouns + 1) % pronouns.length;
+    document.getElementById("playerpronouns").innerText = pronouns[currentpronouns];
+})
+
 var unlockables = new Map([
+    ["woken_up?", false],
     ["basic_resources", true],
     ["battle", false]
 ]);
@@ -65,12 +79,15 @@ var modes = new Map([
     ["battle", false]
 ])
 
+
+
+
+
 var currentEnemy = {
     "id": 0,
     "totHP": 100,
     "curHP": 100
 }
-var turn = 1;
 // loading functions
 function loadEnemy(enemyId) {
     let placeholder = enemies[enemyId];
@@ -117,6 +134,7 @@ document.body.onload = function bodyLoad() {
     document.getElementsByClassName("playerName")[0].innerText = player.name;
     document.getElementsByClassName("PcurHP")[0].innerHTML = player.totHP;
     document.getElementsByClassName("PtotHP")[0].innerHTML = player.totHP;
+    document.getElementById("playerpronouns").innerText = pronouns[currentpronouns];
 
     function PlayMusic() {
         var play = document.getElementById("music");
@@ -126,14 +144,16 @@ document.body.onload = function bodyLoad() {
         PlayMusic();
     });
     load();
+
+    // checks if player has woken up yet
 }
 
 document.querySelectorAll(".playerName").forEach(span => {
     span.addEventListener("click", function changeName() {
-        let newName = prompt("Choose your new name, player:", "Player");
+        let newName = prompt("Choose your new name, player:", "Freya");
         player.name = newName;
         if (newName == "") {
-            newName = "Player";
+            newName = "Freya";
         };
         document.querySelectorAll(".playerName").forEach(span => {
             span.innerText = newName;
@@ -141,17 +161,32 @@ document.querySelectorAll(".playerName").forEach(span => {
     });
 });
 
-function wakeup() {
-    document.getElementById("materialstats").style.opacity = 1;
-    document.getElementById("materialstats").style.height = "160px";
-    document.getElementById("wakeupbutton").remove();
-}
 
+document.getElementById("wakeupbutton").addEventListener("click", function wakeup() {
+    function animation0() {
+        document.getElementById("storytext_div").style.opacity = 0;
+        setTimeout(animation1, 1200)
+    }
+    
+    function animation1() {
+        document.getElementById("actions").style.opacity = 1;
+        document.getElementById("stats").style.opacity = 1;
+        setTimeout(animation2(), 2400);
+    }
+    function animation2() {
+        document.getElementById("materialstats").style.opacity = 1;
+        document.getElementById("materialstats").style.height = "160px";
+    }
+    document.getElementById("wakeupbutton").remove();
+    unlockables.set("woken_up?", true);
+    animation0();
+})
 
 // Saving and loading
 
 function save() {
     var save = {
+        // saves amount of materials and how much per second
         materials_amount: {
             coins: materials[0].amount,
             wood: materials[1].amount,
@@ -162,9 +197,11 @@ function save() {
             wood: materials[1].amount_per_second,
             stone: materials[2].amount_per_second
         },
+        // saves name of player
         name: player.name,
     };
     try {
+        // if successful, converts save variable into a JSON file and saves it to local storage as 'save'
         localStorage.setItem("save",JSON.stringify(save));
         console.log("Saved game:");
         console.log(localStorage.save)
@@ -174,11 +211,13 @@ function save() {
 }
 
 function load() {
+    // accesses localstorage and parses the save state
     var savegame = JSON.parse(localStorage.getItem("save"));
     try {
         localStorage.getItem("save");
         console.log("Saved game:");
         console.log(localStorage.save)
+        // changes player name to saved name :)
         if (typeof savegame.name !== "undefined") player.name = savegame.name;
         if (typeof savegame.name !== "undefined") document.querySelectorAll(".playerName").forEach(span => {
             span.innerText = savegame.name;
@@ -211,5 +250,12 @@ document.body.addEventListener('keypress', function(event) {
         }
     }
 })
+
+
+
+
+/*
+Text imports around story and 
+*/
 import Texts from './JSON/text.json' assert {type: 'json'};
-console.dir(Texts)
+const story = Texts.story_entries;
